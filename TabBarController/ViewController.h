2f150e8c2199edd0f475c7ff472536213e4d3413
apple <apple@appledemacbook-pro.local>
2016-04-27 14:59:17 +0800

    Initial Commit

---
+++ TabBarController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TabBarController
+//
+//  Created by apple on 16/4/27.
+//  Copyright © 2016年 LFX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TabBarController/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TabBarController
+//
+//  Created by apple on 16/4/27.
+//  Copyright © 2016年 LFX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
